1634daa253761601e8416a3670ebca89371e77ac
Tutu699 <166840949+Tutu699@users.noreply.github.com>
2024-04-21 14:26:58 +0700

    PRACTICE_05.sql

--- PRACTICE_05.sql
+++ PRACTICE_05.sql
@@ -12,3 +12,12 @@ FROM emails
 LEFT JOIN texts
 ON emails.email_id = texts.email_id
 AND texts.signup_action = 'Confirmed';
+--EX3:
+SELECT t2.age_bucket,
+round(100*(sum(case when t1.activity_type = 'send' then t1.time_spent else 0 end)/ sum(t1.time_spent)), 2) as send_perc,
+round(100*(sum(case when t1.activity_type = 'open' then t1.time_spent else 0 end) / sum(t1.time_spent)), 2) as open_perc
+FROM activities AS t1 JOIN age_breakdown AS t2
+ON t1.user_id=t2.user_id
+WHERE t1.activity_type IN ('send', 'open') 
+GROUP BY t2.age_bucket
+--EX4:
