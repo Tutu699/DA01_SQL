213a67c58357dceaf79402d06f3b6b04d54c60df
Tutu699 <166840949+Tutu699@users.noreply.github.com>
2024-05-19 22:17:34 +0700

    PRACTICE_10.sql

--- PRACTICE_10.sql
+++ PRACTICE_10.sql
@@ -87,3 +87,69 @@ Join bigquery-public-data.thelook_ecommerce.products as t2 on t1.product_id=t2.i
 Where t1.status='Complete' and t1.delivered_at BETWEEN '2022-01-15 00:00:00' AND '2022-04-16 00:00:00'
 Group by dates, product_categories
 Order by dates
+--06:
+With category_data as
+(Select 
+extract(year from a.created_at) ||'-'|| extract(month from a.created_at) as Month,
+extract(year from a.created_at) as Year,
+b.category as Product_category,
+round(sum(c.sale_price),2) as TPV,
+count(c.order_id) as TPO,
+round(sum(b.cost),2) as Total_cost
+from bigquery-public-data.thelook_ecommerce.orders as a
+Join bigquery-public-data.thelook_ecommerce.products as b on a.order_id=b.id 
+Join bigquery-public-data.thelook_ecommerce.order_items as c on b.id=c.id
+Group by Month, Year, Product_category)
+Select Month, Year, Product_category, TPV, TPO,
+round(cast((TPV - lag(TPV) OVER(PARTITION BY Product_category ORDER BY Year, Month))/lag(TPV) OVER(PARTITION BY Product_category ORDER BY Year, Month) as Decimal)*100.00,2) || '%' as Revenue_growth,
+round(cast((TPO - lag(TPO) OVER(PARTITION BY Product_category ORDER BY Year, Month))/lag(TPO) OVER(PARTITION BY Product_category ORDER BY Year, Month) as Decimal)*100.00,2) || '%' as Order_growth,
+Total_cost,
+round(TPV - Total_cost,2) as Total_profit,
+round((TPV - Total_cost)/Total_cost,2) as Profit_to_cost_ratio
+from category_data
+Order by Product_category, Year, Month
+--07:
+With a as
+(Select user_id, amount, FORMAT_DATE('%Y-%m', first_purchase_date) as cohort_month,
+created_at,
+(Extract(year from created_at) - extract(year from first_purchase_date))*12 + Extract(MONTH from created_at) - extract(MONTH from first_purchase_date) +1 as index
+from 
+(Select user_id, 
+round(sale_price,2) as amount,
+Min(created_at) OVER (PARTITION BY user_id) as first_purchase_date,
+created_at
+from bigquery-public-data.thelook_ecommerce.order_items) as b),
+cohort_data as
+(Select cohort_month, 
+index,
+COUNT(DISTINCT user_id) as user_count,
+round(SUM(amount),2) as revenue
+from a
+Group by cohort_month, index
+ORDER BY INDEX),
+--CUSTOMER COHORT-- 
+Customer_cohort as
+(Select 
+cohort_month,
+Sum(case when index=1 then user_count else 0 end) as m1,
+Sum(case when index=2 then user_count else 0 end) as m2,
+Sum(case when index=3 then user_count else 0 end) as m3,
+Sum(case when index=4 then user_count else 0 end) as m4
+from cohort_data
+Group by cohort_month
+Order by cohort_month),
+--RETENTION COHORT--
+retention_cohort as
+(Select cohort_month,
+round(100.00* m1/m1,2) || '%' as m1,
+round(100.00* m2/m1,2) || '%' as m2,
+round(100.00* m3/m1,2) || '%' as m3,
+round(100.00* m4/m1,2) || '%' as m4
+from customer_cohort)
+--CHURN COHORT--
+Select cohort_month,
+(100.00 - round(100.00* m1/m1,2)) || '%' as m1,
+(100.00 - round(100.00* m2/m1,2)) || '%' as m2,
+(100.00 - round(100.00* m3/m1,2)) || '%' as m3,
+(100.00 - round(100.00* m4/m1,2))|| '%' as m4
+from customer_cohort
